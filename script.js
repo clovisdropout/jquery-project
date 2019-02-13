f345956631e025202e9cc0e793dfcafced14ddb3
Matteo <matteo.delas7@icloud.com>
2019-02-13 09:38:52 +0100

    add header

--- script.js
+++ script.js
@@ -11,12 +11,24 @@ $(window).on("scroll",function(){
         $("#scrollTop").css("opacity", 0);
     }
     
-    
+
 
 
 })
 
 
 // lorsque positionY est supérieur à 100 
-    // résuire heugth de la nav
+    // résuire heigth de la nav
     // positionner la nav en fixe en haut de la page.
+
+    window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("nav").style.padding = "30px 10px";
+    document.getElementById("link").style.fontSize = "25px";
+  } else {
+    document.getElementById("nav").style.padding = "80px 10px";
+    document.getElementById("link").style.fontSize = "35px";
+  }
+}
